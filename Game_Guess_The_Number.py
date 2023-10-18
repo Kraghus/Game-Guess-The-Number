c5660d64fd86e438061358306a4cfb0e6b18b4db
Kraghus <44181473+Kraghus@users.noreply.github.com>
2023-10-18 19:53:51 -0400

    Update Game_Guess_The_Number.py
    Added support for player names.

--- Game_Guess_The_Number.py
+++ Game_Guess_The_Number.py
@@ -1,6 +1,13 @@
 
+# import random number generation capabilities
 import random
 
-random_number = random.randint(1, 100)
+# get the player name
+print('Please enter your name: ', end='')
+player_name = input()
 
-print(random_number)
+print('Hello, ' + player_name + '! Let\'s play a game! I\'m thinking of a random number between 1 and 100. Try to guess what it is!')
+
+
+random_number = random.randint(1, 100)
+# print(random_number)
