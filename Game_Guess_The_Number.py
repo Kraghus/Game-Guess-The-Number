f31ac2a5432fef630c62310796a78a92bb9a91ce
Kraghus <44181473+Kraghus@users.noreply.github.com>
2023-10-19 17:21:00 -0400

    Update Game_Guess_The_Number.py
    Refactoring.

--- Game_Guess_The_Number.py
+++ Game_Guess_The_Number.py
@@ -3,11 +3,8 @@ import random # import random number generation capabilities
 import sys # used to exit the game after completion
 
 
-# declare variables
-player_guess = 0
-guess_number = 0
-number_of_guesses_left = 7
-random_number = 0
+# declare global constant
+CONST_MAX_GUESSES = 7
 
 # get the player name
 print('Please enter your name: ', end='')
@@ -20,8 +17,12 @@ print('')
 
 # get the random number
 random_number = random.randint(1, 100)
-#print(random_number) # for testing purposes
-#print('')      
+print(random_number) # for testing purposes
+print('')
+
+number_of_guesses_left = CONST_MAX_GUESSES
+guess_number = 0
+player_guess = 0
 
 # continues play until the maximum number of guesses has been reached
 while number_of_guesses_left > 0:
@@ -30,27 +31,42 @@ while number_of_guesses_left > 0:
     print('Enter guess #' + str(guess_number) + ': ', end='')
     player_guess = input()
     
-    # compare the guess against the random number
-    if int(player_guess) == random_number:
-        print('Congratulations! You have correctly guessed the number ' + str(random_number) + ' in ' + str(guess_number) + ' guess(es) and won the game!')
-        print('')
-        sys.exit(0)
-
-    elif int(player_guess) > random_number:
+    # compare the guess with the actual number, give feedback to the player, and use up a guess
+    if int(player_guess) > random_number:
         print('That guess was too high...')
         print('')
         number_of_guesses_left -= 1
         continue
     
+    # compare the guess with the actual number, give feedback to the player, and use up a guess
     elif int(player_guess) < random_number:
         print('That guess was too low...')
         print('')
         number_of_guesses_left -= 1
         continue
     
-print('You have exceeded the maximum number of guesses and lost the game... the number was ' + str(random_number) + '! Better luck next time, ' + player_name + '!')
-print('')
-sys.exit(0)
+    # this code only executes if the player guesses the correct number
+    else:
+        break    
+
+
+# if the player guesses the correct number, print the message for a win
+if int(player_guess) == random_number:
+    if guess_number == 1:
+        print ('Congratulations, ' + player_name + '! You have correctly guessed the number ' + str(random_number) + ' in ' + str(guess_number) + ' guess and won the game!')
+        print('')
+        sys.exit
+        
+    else:
+        print('Congratulations, ' + player_name + '! You have correctly guessed the number ' + str(random_number) + ' in ' + str(guess_number) + ' guesses and won the game!')
+        print('')
+        sys.exit(0)
+
+# if the player does not guess the correct number, print the message for a loss
+else:
+    print('You have exceeded the maximum number of guesses and lost the game... the number was ' + str(random_number) + '! Better luck next time, ' + player_name + '!')
+    print('')
+    sys.exit(0)
     
 
     
