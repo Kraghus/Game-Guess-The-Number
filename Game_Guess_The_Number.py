47340fd6be65ddd9937b909e09e818758fcbf976
Kraghus <44181473+Kraghus@users.noreply.github.com>
2023-10-21 05:24:36 -0400

    Update Game_Guess_The_Number.py
    Fixed a bug that would prevent the player from playing another game after invalid input was entered at the end of a game.

--- Game_Guess_The_Number.py
+++ Game_Guess_The_Number.py
@@ -5,14 +5,11 @@ import random # import random number generation capabilities
 CONST_MAX_GUESSES = 7
 
 # get the player name
-print('Please enter your name: ', end='')
+print("Please enter your name: ", end="")
 player_name = input()
-print('')
 
 # greet the player and explain the rules of the game
-print('Hello, ' + player_name + '! Let\'s play a game! I\'m thinking of a random number between 1 and 100. Try to guess what it is!')
-print('')
-
+print("\nHello, " + player_name + "! Let's play a game! I'm thinking of a random number between 1 and 100. Try to guess what it is!\n")
 
 # declare variables for the application loop, number of guesses a player has used, and the integer that they guessed
 bool_continue_game = True
@@ -31,25 +28,22 @@ while bool_continue_game == True:
         try:
             # get the guess from the player
             guess_number += 1
-            print('Enter guess #' + str(guess_number) + ': ', end='')
+            print("Enter guess #" + str(guess_number) + ": ", end="")
             player_guess = input()
     
             # compare the guess with the actual number, give feedback to the player, and use a guess
             if int(player_guess) > random_number:
-                print('That guess was too high...')
-                print('')
+                print("That guess was too high...\n")
                 continue
     
             # compare the guess with the actual number, give feedback to the player, and use a guess
             elif int(player_guess) < random_number:
-                print('That guess was too low...')
-                print('')
+                print("That guess was too low...\n")
                 continue
     
         # catch any invalid input and give feedback to the player
         except:
-            print('Invalid input detected, please make sure your guess is a whole number.')
-            print('')
+            print("Invalid input detected, please make sure your guess is a whole number.\n")
         
             # give the player their guess back in case they wasted it on invalid input
             guess_number -= 1
@@ -58,26 +52,20 @@ while bool_continue_game == True:
         else:
             break    
 
-
     # if the player guessed the correct number, print the message for a win
     if int(player_guess) == random_number:
         if guess_number == 1:
-            print('')
-            print ('Congratulations, ' + player_name + '! You have correctly guessed the number ' + str(random_number) + ' in ' + str(guess_number) + ' guess and won the game!')
-            print('')
+            print ("\nCongratulations, " + player_name + "! You have correctly guessed the number " + str(random_number) + " in " + str(guess_number) + " guess and won the game!\n")
         
         else:
-            print('')
-            print('Congratulations, ' + player_name + '! You have correctly guessed the number ' + str(random_number) + ' in ' + str(guess_number) + ' guesses and won the game!')
-            print('')
+            print("\nCongratulations, " + player_name + "! You have correctly guessed the number " + str(random_number) + " in " + str(guess_number) + " guesses and won the game!\n")
 
     # if the player did not guess the correct number, print the message for a loss
     else:
-        print('You have exceeded the maximum number of guesses and lost the game... the number was ' + str(random_number) + '! Better luck next time, ' + player_name + '!')
-        print('')
+        print("You have exceeded the maximum number of guesses and lost the game... the number was " + str(random_number) + "! Better luck next time, " + player_name + "!\n")
 
     # ask the player if they would like to play again
-    print('Would you like to play again, ' + player_name + '? (Y for YES, N for NO): ', end='')
+    print("Would you like to play again, " + player_name + "? (Y for YES, N for NO): ", end="")
     play_again = input()
     
     while play_again != 'Y' or play_again != 'Y' or play_again != 'N' or play_again != 'n':
@@ -86,24 +74,18 @@ while bool_continue_game == True:
         if play_again == 'Y' or play_again == 'y': 
             guess_number = 0
             player_guess = 0
-            print('')
-            print('Ok! I\'m thinking of another number between 1 and 100. Try to guess what it is!')
-            print('')
+            print("\nOk! I'm thinking of another number between 1 and 100. Try to guess what it is!\n")
             break
     
         # terminate the overall game loop, thus ending the game
         elif play_again == 'N' or play_again == 'n':
             bool_continue_game = False
-            print('')
-            print('Ending game... catch you later, ' + player_name + '!')
-            print('')
+            print("\nEnding game... catch you later, " + player_name + "!\n")
             break
         
         # execute repeatedly for invalid input
         else:
-            bool_continue_game = False
-            print('')
-            print('Invalid input detected. Would you like to play again, ' + player_name + '? (Y for YES, N for NO): ', end='')
+            print("\nInvalid input detected. Would you like to play again, " + player_name + "? (Y for YES, N for NO): ", end="")
             play_again = input()
             continue
         
